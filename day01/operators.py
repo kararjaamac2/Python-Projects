4721530f5db0d7d03d72cfffe4a2cd42404b4db4
Abdulahi Jama <kararjaamac1@gmail.com>
2024-02-06 10:14:36 -0800

    arithmetical  operations

---
+++ day01/operators.py
@@ -0,0 +1,18 @@
+#arthmetic, +, - , *, /, %
+
+print(10 -2)
+print(10 +2)
+print(10 *2)
+print(10 %3)
+print(int (100 /2))
+
+#ShortHand operation
+a=200
+print(a)
+a+=100
+print(a)
+a-= 50
+print(a)
+
+# Relational operations
+
