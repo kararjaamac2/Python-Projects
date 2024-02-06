509f6960e9d67eb7388e341f026a3bc0e7e3ad15
Abdulahi Jama <kararjaamac1@gmail.com>
2024-02-06 08:43:14 -0800

    This is Concatenation day1

---
+++ day01/CONCATENATION.py
@@ -0,0 +1,8 @@
+name = 'jama'
+age = 26
+#print("my name is " + name)
+print('my age is ' +str (age))
+
+#print('my age is {}'.format(age))
+print('My name is  {} and I am {} years old'.format(name, age))
+print(f'my is name {name} and i am {age} years old')
---
+++ day01/hello_world.py
@@ -0,0 +1,33 @@
+print('hellow world') # this is single line
+print("yes")
+"""
+this is python
+i am learning
+
+"""
+print('Cydeo\nschool') # two lines
+print('\tpython program') # space
+print('i love python')
+# python it is dynamic date type
+# it means you can store any data type
+name= 'jama'
+print(name)
+print(type(name))
+age= 25
+print(age)
+print(type(age))
+gpc= 3.8
+print(type(gpc))
+is_employee= True
+print(is_employee)
+# casting in python you have to use constractor
+
+s = '25'
+#str(s)
+s =int(s)
+print( s * 5)
+
+
+
+
+
